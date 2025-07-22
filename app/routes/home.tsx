39d68895a09ec4d67760e68bbb2aabd72f2112b1
vaibhav kottari <kottarivaibhav@gmail.com>
2025-07-22 11:23:23 +0530

    second phase change

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,33 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import {resumes} from "../../constants";
+import ResumeCard from "../components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumecheck" },
+    { name: "description", content: "Smart Feedback towards your dream" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')]">
+    <Navbar />
+    <section className='main-section'>
+      <div className="page-heading"></div>
+      <h1>Track your Application and Resume ratings</h1>
+      <h2>Review your submission and check AI Powered Feedback</h2>
+    </section>
+
+    {resumes.length > 0 && (
+      <div className="resumes-section">
+       {resumes.map((resume)=>(
+      <ResumeCard key={resume.id} resume={resume} />
+    ))}
+    </div>
+    )}
+    
+
+   
+    </main>
 }
