2759a3687723bd97d57849bc556ff6b35034654d
vaibhav kottari <kottarivaibhav@gmail.com>
2025-07-28 18:45:17 +0530

    vercel edits

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  ssr: true,
-  serverBuildFile: "index.js",
+  ssr: false, // Start with SPA for easier deployment
+  future: {
+    unstable_optimizeDeps: true,
+  },
 } satisfies Config;
