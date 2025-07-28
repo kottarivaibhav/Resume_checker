346e2d0fedb1cce992bbcf9040bb709fae4c45c5
vaibhav kottari <kottarivaibhav@gmail.com>
2025-07-28 09:26:08 +0530

    modified files

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,9 +1,25 @@
-import { Link } from 'react-router';
-import ScoreCircle from './ScoreCircle';
+import {Link} from "react-router";
+import ScoreCircle from "~/components/ScoreCircle";
+import {useEffect, useState} from "react";
+import {usePuterStore} from "~/lib/puter";
 
-const ResumeCard = ({resume : {id,companyName,jobTitle,feedback,imagePath}}:{resume:Resume}) => {
-  return (
-    <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
+const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath } }: { resume: Resume }) => {
+    const { fs } = usePuterStore();
+    const [resumeUrl, setResumeUrl] = useState('');
+
+    useEffect(() => {
+        const loadResume = async () => {
+            const blob = await fs.read(imagePath);
+            if(!blob) return;
+            let url = URL.createObjectURL(blob);
+            setResumeUrl(url);
+        }
+
+        loadResume();
+    }, [imagePath]);
+
+    return (
+        <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
             <div className="resume-card-header">
                 <div className="flex flex-col gap-2">
                     {companyName && <h2 className="!text-black font-bold break-words">{companyName}</h2>}
@@ -14,20 +30,18 @@ const ResumeCard = ({resume : {id,companyName,jobTitle,feedback,imagePath}}:{res
                     <ScoreCircle score={feedback.overallScore} />
                 </div>
             </div>
-            
+            {resumeUrl && (
                 <div className="gradient-border animate-in fade-in duration-1000">
                     <div className="w-full h-full">
                         <img
-                            src={imagePath}
+                            src={resumeUrl}
                             alt="resume"
                             className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
                         />
                     </div>
                 </div>
-               
+                )}
         </Link>
-      
-)
+    )
 }
-
 export default ResumeCard
